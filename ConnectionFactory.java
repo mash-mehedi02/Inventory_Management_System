a7868e69ff4daee1c82a9213b9a140cafed85ef6
developer-mashmehedi <mehedihasan110571@gmail.com>
2025-01-08 00:55:48 +0600

    final update

---
+++ ConnectionFactory.java
@@ -0,0 +1,59 @@
+import java.sql.*;
+
+public class ConnectionFactory {
+
+    private Connection conn = null;
+
+
+    public ConnectionFactory() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/INVENT", "root", "Mehedi1105");
+            System.out.println("Connection established successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConn() {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/INVENT", "root", "Mehedi1105");
+                System.out.println("Connected successfully.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
+    public boolean checkLogin(String username, String password, String userType) {
+        String query = "SELECT * FROM users WHERE username = ? AND password = ? AND usertype = ? LIMIT 1";
+        ResultSet resultSet = null;
+
+        try (PreparedStatement stmt = getConn().prepareStatement(query)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.setString(3, userType);
+
+            resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close ResultSet
+            try {
+                if (resultSet != null) resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+}
